d9e67da3d1b568a0feb998c5b01521c226db3f85
Francisco Sola Porcuna <fran_sopo@hotmail.com>
2025-06-30 16:47:03 +0200

    Add CRUD module for Entidad and update configurations
    Implements a new Entidad module with a controller, service, DTOs, and testing setup to enable basic CRUD operations. Updates `mis-entidades` module to include the Entidad module. Enhances `.prettierrc` to enforce end-of-line rules and modifies `package.json` to include `@nestjs/mapped-types`. Renames `eslint.config.mjs` for clarity.

--- libs/mis-entidades/src/mis-entidades.module.ts
+++ libs/mis-entidades/src/mis-entidades.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MisEntidadesService } from './mis-entidades.service';
+import { EntidadModule } from './entidad/entidad.module';
 
 @Module({
   providers: [MisEntidadesService],
   exports: [MisEntidadesService],
+  imports: [EntidadModule],
 })
 export class MisEntidadesModule {}
